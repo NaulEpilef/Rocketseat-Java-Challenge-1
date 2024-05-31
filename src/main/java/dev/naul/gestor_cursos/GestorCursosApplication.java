b4e2c1da966a0b107c8acaf7570abaa1509ad1de
NaulEpilef <lfps1999@gmail.com>
2024-05-31 17:25:43 -0300

    First commit

---
+++ src/main/java/dev/naul/gestor_cursos/GestorCursosApplication.java
@@ -0,0 +1,13 @@
+package dev.naul.gestor_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/naul/gestor_cursos/GestorCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.naul.gestor_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
